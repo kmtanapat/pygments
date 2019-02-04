1ce0671afb8dfe43db6bf30dab907b48b1438257
kmtanapat <km.tanapat@gmail.com>
2019-02-04 17:14:03 +0700

    finished api

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework.schemas import get_schema_view # new
 from snippets import views
+schema_view = get_schema_view(title='Pastebin API') # new
 
 urlpatterns = [
+    path('schema/', schema_view),  # new
     path('snippets/', views.SnippetList.as_view(), name='snippet-list'),
     path('snippets/<int:pk>/', views.SnippetDetail.as_view(), name='snippet-detail'),
     path('users/', views.UserList.as_view(), name='user-list'),
